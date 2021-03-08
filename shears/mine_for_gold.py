b9b67b90b4b2becce2eadc3c66f82d5a221148bb
jessrenteria <jessrenteria@users.noreply.github.com>
2021-03-07 17:25:53 -0800

    Add exploratory binary for Shears.
    This binary authenticates a Reddit API connection via PRAW and scrapes
    the latest comments from /r/wallstreetbets.

---
+++ shears/mine_for_gold.py
@@ -0,0 +1,52 @@
+"""Exploratory binary for analyzing subreddit comments.
+
+Requires authentication via command line arguments.
+"""
+import argparse
+import praw
+
+
+_USERNAME = 'projectmerino'
+_USER_AGENT = (
+    'python:com.projectmerino.exploration:v0 '
+    '(by /u/projectmerino)'
+)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Mine for gold.')
+    parser.add_argument(
+        '--client_id',
+        type=str,
+        help='Reddit client id.'
+    )
+    parser.add_argument(
+        '--client_secret',
+        type=str,
+        help='Reddit client secret.'
+    )
+    parser.add_argument(
+        '--password',
+        type=str,
+        help='Reddit password.'
+    )
+    args = parser.parse_args()
+
+    print('Authenticating Reddit API connection...')
+    reddit = praw.Reddit(
+        client_id=args.client_id,
+        client_secret=args.client_secret,
+        username=_USERNAME,
+        password=args.password,
+        user_agent=_USER_AGENT,
+    )
+    print('Authenticated!')
+
+    print('Mining for gold...')
+    for comment in reddit.subreddit('wallstreetbets').comments():
+        print(comment.author.name + ' says:\n')
+        print(comment.body)
+
+
+if __name__ == '__main__':
+    main()
